d9f9be0220f8340e9298fddda79a4f3b91b19f50
Franca <franciedigital@gmail.com>
2022-10-18 07:16:17 -0700

    Function to print _putchar

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+New README.md from 0x02-functions_nested_loops
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+ *  * print_alphabt - print the lower-case alphabet on one line.
+ */
+int _putchar(char c);
+
+
+#endif
