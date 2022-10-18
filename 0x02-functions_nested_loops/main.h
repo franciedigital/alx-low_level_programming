7d25763fbfaf6c0c70fb6d58eed7caea88aad305
Franca <franciedigital@gmail.com>
2022-10-18 07:26:25 -0700

    Function that prints alphabet in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 /**
- *  * print_alphabt - print the lower-case alphabet on one line.
+ *  * print_alphabet - print the lower-case alphabet on one line.
  */
 int _putchar(char c);
-
+void print_alphabet(void);
 
 #endif
