b5e94859598894981fb725d2d85f76252dd7cf74
Franca Mgbogu <franciedigital@gmail.com>
2023-05-15 14:02:29 +0100

    The time complexity (average case) of a jump search in an array of size n, using step = sqrt(n)

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -49,5 +49,6 @@ void free_skiplist(skiplist_t *);
 int linear_search(int *, size_t, int);
 int binary_search(int *, size_t, int);
 int jump_search(int *, size_t, int);
+int interpolation_search(int *, size_t, int);
 
 #endif
